87a625d93945b88d6d85e941f04839959ec43891
Saad M Malik <17054627+saad-malik-codes@users.noreply.github.com>
2022-01-13 01:32:24 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,37 +1,8 @@
+// In your gatsby-config.js
+const path = require('path');
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
-  },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-	{
       resolve: '@builder.io/gatsby',
       options: {
         // public API Key
@@ -52,11 +23,20 @@ module.exports = {
             /* ... */
           };
         },
-
+        // optional, to resolve a single entry to multiple, for e.g in localization
+        resolveDynamicEntries: async (entries) => {
+          const entriesToBuild = []
+          for entry of entries {
+            if (entry.data.myprop.isDynamic){
+               entriesToBuild.push(await myEntryResolver(entry))
+            }
+            else {
+               entriesToBuild.push(entry)
+            }
+          }
+          return entriesToBuild;
+        },
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
-}
+};
